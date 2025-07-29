dd4167120d8f778a5be2466cca6213147e1fdbf1
ThiagoLaredo <thiago@olatu.com.br>
2025-07-29 18:56:45 -0300

    swiper

--- src/js/modules/swiper-destaques.js
+++ src/js/modules/swiper-destaques.js
@@ -5,40 +5,31 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-
 export class SwiperDestaques {
-  constructor(containerSelector, options = {}) {
+  constructor(containerSelector = '.swiper-destaques', options = {}) {
+    this.container = document.querySelector(containerSelector);
     this.options = {
       modules: [Navigation, Pagination, Scrollbar],
-      slidesPerView: 3,
-      // spaceBetween: 20,
-      loop: false, // loop desativado para scrollbar funcionar corretamente
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: false,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
       },
       scrollbar: {
         el: '.swiper-scrollbar',
         draggable: true,
       },
-      breakpoints: {
-        640: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        }
-      },
       ...options
-    };    
+    };
   }
 
   init() {
-    if (!this.container) return;
+    if (!this.container) {
+      console.error('Container do Swiper não encontrado:', this.container);
+      return;
+    }
 
     this.swiper = new Swiper(this.container, this.options);
     return this.swiper;
--- src/js/modules/swiper-gallery.js
+++ src/js/modules/swiper-gallery.js
@@ -1,29 +1,36 @@
 import { Swiper } from 'swiper';
-import { Autoplay, FreeMode } from 'swiper/modules';
+import { Autoplay, FreeMode, Scrollbar } from 'swiper/modules'; // Removi o Touch
 import 'swiper/css';
+import 'swiper/css/scrollbar';
 
 export class SwiperGallery {
-  constructor(containerSelector, options = {}) {
+  constructor(containerSelector = '.swiper-galeria', options = {}) {
     this.container = document.querySelector(containerSelector);
     this.options = {
-      modules: [Autoplay, FreeMode], // Removido Loop dos módulos
+      modules: [Autoplay, FreeMode, Scrollbar], // Removi o Touch
       slidesPerView: 'auto',
       spaceBetween: 20,
-      // loop: true,
-      // autoplay: {
-      //   delay: 1000,
-      //   disableOnInteraction: false,
-      // },
+      loop: true,
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+      },
       speed: 800,
       freeMode: true,
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
       watchSlidesProgress: true,
+      // Configurações de toque (opcional)
+      touchEventsTarget: 'container', // Garante que o toque funcione no container
+      grabCursor: true, // Muda o cursor para "mão" quando arrastável
       ...options
     };
   }
 
   init() {
     if (!this.container) return;
-
     this.swiper = new Swiper(this.container, this.options);
     return this.swiper;
   }
